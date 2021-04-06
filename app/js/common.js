857460ebc39f1cc357281e03a9e03f098a39def6
Viktoria <zaharova181114@gmail.com>
2021-04-06 21:54:39 +0300

    mail php

--- app/js/common.js
+++ app/js/common.js
@@ -83,3 +83,19 @@ $(function () {
     });
 });
 //end
+
+// mail
+$(".form").submit(function (e) {
+    e.preventDefault();
+
+    $.ajax({
+        type: "POST",
+        url: "mail.php",
+        data: $(this).serialize()
+    }).done(function () {
+        $(this).find("input").val("");
+        alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+        $(".form").trigger("reset");
+    });
+    return false;
+});
